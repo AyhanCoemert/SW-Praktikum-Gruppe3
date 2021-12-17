6df4b00abea0e694bff1af5ca3daf0bfb9010420
Vincent Flemming <vf018@hdm-stuttgart.de>
2021-12-17 15:00:27 +0100

    Student.py vorerst

--- src/server/bo/Studiengang.py
+++ src/server/bo/Studiengang.py
@@ -11,3 +11,38 @@ class Studiengang(bo.BusinessObject):
         self._semester = 0
         self._name = 0
 
+    def get_ID(self):
+        return self._ID
+
+    def set_ID(self, ID):
+        self.ID = ID
+
+    def get_module(self):
+        return self._module
+
+    def set_module(self, module):
+        self._module = module
+
+    def get_semester(self):
+        return self._semester
+
+    def set_semester(self, semester):
+        self._semester = semester
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    # Erstellung von Studiengang mit Dictionary
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        obj = Modul()
+        obj.set_id(dictionary["id"])
+        obj.set_creation_date(dictionary["creation_date"])
+        obj.set_ID(dictionary["ID"])
+        obj.set_module(dictionary["module"])
+        obj.set_semester(dictionary["semester"])
+        obj.set_name(dictionary["name"])
+        return obj                         # nochmal kontrollieren
