cd394a44ba32099dffb08f26bfa8c9ec323d0c9d
soumayyahaboubakar <94307543+soumayyahaboubakar@users.noreply.github.com>
2022-01-05 12:30:36 +0100

    Aktualisierung

--- Frontend/Api/SPO.js
+++ Frontend/Api/SPO.js
@@ -7,16 +7,17 @@ import MaterialTable from 'material-table'
 function SPO(){
     const [tableData, setTableData] = useState ([
      {Semester:"1", EDV_Nummer:"335000", Modul:"Einstufungstest Englisch", SWS:"0", ECTS:"0", Prüfungsform:"LÜ"},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
-     {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
+     {Semester:"1", EDV_Nummer:"335120", Modul:"Marketing & Organisation", SWS:"4", ECTS:"5", Prüfungsform:"KL"},
+     {Semester:"1", EDV_Nummer:"335121", Modul:"Grundlagen Wirtschaftsinformatik", SWS:"4", ECTS:"5", Prüfungsform:"KMP"},
+     {Semester:"1", EDV_Nummer:"335122", Modul:"Datenbanken Grundlagen", SWS:"4", ECTS:"5", Prüfungsform:"KMP"},
+     {Semester:"1", EDV_Nummer:"335123", Modul:"Programmieren", SWS:"4", ECTS:"5", Prüfungsform:"KMP"},
+     {Semester:"1", EDV_Nummer:"335126", Modul:"Propädeutik WI", SWS:"4", ECTS:"5", Prüfungsform:"KMP"},
+     {Semester:"1", EDV_Nummer:"338048", Modul:"Data Literacy_1", SWS:"2", ECTS:"5", Prüfungsform:"KSP"},
+     {Semester:"2", EDV_Nummer:" 338047", Modul:"Data Literacy_2", SWS:"2", ECTS:"5", Prüfungsform:"KMP"},
+     {Semester:"2", EDV_Nummer:"335125", Modul:"Externes und Internes_Rechnungswesen", SWS:"4", ECTS:"5", Prüfungsform:"KL"},
+     {Semester:"2", EDV_Nummer:"335127", Modul:"Algorithmen & Datenstrukturen", SWS:"4", ECTS:"5", Prüfungsform:"KMP"},
+     {Semester:"2", EDV_Nummer:"335128", Modul:"Geschäftsprozesse", SWS:"4", ECTS:"5", Prüfungsform:"KL"},
+     {Semester:"2", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
      {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
      {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
      {Semester:"", EDV_Nummer:"", Modul:"", SWS:"", ECTS:"", Prüfungsform:""},
@@ -35,7 +36,7 @@ function SPO(){
           <h1 align="center">SPOtch</h1>
           <h4 align="center"></h4>
 
-          <MaterialTable columns = {columns} data={tableData}/>
+          <MaterialTable columns = {columns} data={tableData} title="WI SPO"/>
        
         </div>
       );
