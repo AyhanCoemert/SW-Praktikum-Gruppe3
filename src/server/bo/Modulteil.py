bae225da2dd7641a1c9be5abaaa79c6c503ec0e3
Vincent Flemming <vf018@hdm-stuttgart.de>
2022-01-05 16:39:05 +0100

    Modulteil.py

---
+++ src/server/bo/Modulteil.py
@@ -0,0 +1,26 @@
+from src.server.bo import BusinessObject as bo
+
+
+# Group Invitation Klasse
+class Modulteil(bo.BusinessObject):
+
+    def __init__(self):
+        super().__init__()
+        self._ID = 0
+
+
+    def get_ID(self):
+        return self._ID
+
+    def set_ID(self, ID):
+        self.ID = ID
+
+
+
+    # Erstellung von Modulteil mit Dictionary
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        obj = Modulteil()
+        obj.set_id(dictionary["id"])
+        obj.set_creation_date(dictionary["creation_date"])
+        return obj                          # nochmal kontrollieren
