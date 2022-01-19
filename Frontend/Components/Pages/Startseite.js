2209422765688672467bf351ab7bc38c6b739b23
MonikaVasileva <94307816+MonikaVasileva@users.noreply.github.com>
2022-01-19 18:36:40 +0100

    Update Startseite.js

--- Frontend/Components/Pages/Startseite.js
+++ Frontend/Components/Pages/Startseite.js
@@ -1,1 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import './index.css';
+import './logo.svg';
+import Anmelden from './components/Registrierung';
+import SPO from './API/SPO'
+
+
+class Homepage extends Component {
+
+  render() {
+
+    return (
+      <div class="App-header">
+		<Anmelden>Anmelden</Anmelden>
+		<SPO>Dein online SPO</SPO>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
+
 
