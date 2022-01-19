9b1aceec27ec35413b364560a3d2d3e32613e76e
soumayyahaboubakar <94307543+soumayyahaboubakar@users.noreply.github.com>
2022-01-19 18:58:37 +0100

    HTML

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,22 +1,9 @@
-<<<<<<< Updated upstream:Frontend/App.js
-import * as React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
-=======
 import React from 'react';
 import Registrierung from './Components/Pages/Registrierung';
 
 function App() {
   return <Registrierung/>
    
-  
->>>>>>> Stashed changes:spotch/src/App.js
 }
 
 
