19ca87f8b8f3f81db9419dc79989002b7258c466
soumayyahaboubakar <94307543+soumayyahaboubakar@users.noreply.github.com>
2022-01-27 17:35:25 +0100

    Update

--- Frontend/spotch/src/firebase.js
+++ Frontend/spotch/src/firebase.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase/compat/app';
+/* import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 //@author [Soumayyah Aboubakar](https://github.com/soumayyahaboubakar)
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN ,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID ,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET ,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID ,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 
 export const auth = app.auth()
-export default app 
+export default app  */
  
 
 
@@ -23,19 +23,19 @@ export default app
 
 
 
-/* import { initializeApp } from "firebase/compat/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.React_App_Firebase_Api_Key,
-    authDomain: process.env.React_App_Firebase_AuthDomain ,
-    projectId: process.env.React_App_Firebase_ProjectId ,
-    storageBucket: process.env.React_App_Firebase_StorageBucket ,
-    messagingSenderId: process.env.React_App_Firebase_MessagingSenderId ,
-    appId:process.env.React_App_Firebase_AppId 
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
   
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app); */
+export const auth = getAuth(app);
--- Frontend/spotch/src/firebaseConfig.js
+++ Frontend/spotch/src/firebaseConfig.js
@@ -5,11 +5,11 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN ,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID ,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET ,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID ,
-    appId:process.env.REACT_APP_FIREBASE_APPID
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+    appId:process.env.REACT_APP_FIREBASE_APP_ID
   
 };
 
