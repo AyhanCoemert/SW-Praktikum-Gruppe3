403bffc0272a407cc3fd96e00a5360eeab48d941
MonikaVasileva <94307816+MonikaVasileva@users.noreply.github.com>
2022-01-27 17:38:48 +0100

    Module neu

--- Frontend/spotch/src/Api/Modulteil.js
+++ Frontend/spotch/src/Api/Modulteil.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+/* /import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 
@@ -10,6 +10,50 @@ function App() {
     }
 
     )
+}
+
+
+ */
+import BusinessObject from "./BusinessObject.js"
+
+
+
+export default class Modulteil extends BusinessObject{
+    /**
+   * @param {*} ID - ID des Modulteils
+   */
+
+    constructor(ID) {
+        super();
+        this.ID = ID
+
+    }
+    
+
+    setID(ID) {
+       this.ID = ID; 
+    }
+
+    getID() {
+       return this.ID; 
+    }
+
+    static fromJSON(modulteil) {
+        let result = [];
+
+        if (Array.isArray(modulteil)) {
+            user.forEach((c) => {
+                Object.setPrototypeOf(c, Modulteil.prototype);
+                result.push(c);
+            })
+        } else {
+            let c = modulteil;
+            Object.setPrototypeOf(c, Modulteil.prototype);
+            result.push(c);
+        }
+
+        return result;
+    }
 }
 
 
